bc7e24ded4206ab14ce3d5d4356ffc6538306b44
kaitoAckerman <k4170.4ck3rm4n@gmail.com>
2018-05-01 13:05:11 +0800

    Member badge list!
    Display all contributors for sig-fsm repository.

--- assets/js/members-board.js
+++ assets/js/members-board.js
@@ -1,6 +1,6 @@
 $.getJSON("https://api.github.com/repos/SiG-FSTM/sig-fstm.github.io/contributors", function (data) {
 
-	$.each(data, function (i, field) {
-		$("#members").append("<div class='member-badge'>" + field.login + "</div>");
+	$.each(data, function (i, member) {
+		$("#members").append("<a href='"+ member.html_url +"' target='_blank'><div class='member-badge'><img class='avatar' src='" + member.avatar_url + "'><div class='info'><p>" + member.login + "</p><p>" + member.contributions + " contributions!</p></div></div></a>");
 	});
 });
